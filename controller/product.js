ab5e0982c47576c1bfce91ad8a4ba5412b5f7521
ADESANYA JOSHUA AYODEJI <adesanyajoshua@ymail.com>
2020-08-07 17:03:01 +0100

    Revert "Revert "feat - added update product logic""

--- controller/product.js
+++ controller/product.js
@@ -21,3 +21,25 @@ exports.getProducts = async (req, res) => {
         return errorResMsg(res, 500, err);
     }
 }
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        })
+        return successResMsg(res, 200, product);
+    } catch (err) {
+        return errorResMsg(res, 500, err);
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete({
+            _id: req.params.id
+        })
+        return successResMsg(res, 200, product);
+    } catch (err) {
+        return errorResMsg(res, 500, err);
+    }
+}
--- routes/product.js
+++ routes/product.js
@@ -6,8 +6,11 @@ const {
     validation
 } = require("../middleware/validation")
 
-router.post('/create', productValidationRules(), validation, userController.addProduct);
+
 router.get('/', userController.getProducts);
+router.post('/', productValidationRules(), validation, userController.addProduct);
+router.patch('/:id', productValidationRules(), validation, userController.updateProduct);
+router.delete('/:id', userController.deleteProduct);
 
 
 
