5f3a47d7fa2416aa5a499c0f8e37b6c61c659797
Joshua Adesanya <adesanyajoshua@ymail.com>
2020-08-10 13:22:05 +0100

    bug: logfix

--- controller/log.js
+++ controller/log.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const Log = require('../model/log');
 const {
     successResMsg,
@@ -20,7 +21,9 @@ exports.getLog = async (req, res) => {
 exports.getUserLog = async (req, res) => {
     try {
         console.log(req.params.id)
-        const Logs = await Log.find({ user: req.params.id}).populate("user").populate("product").sort({
+        const Logs = await Log.find({
+            user: req.params.id
+        }).populate("user").populate("product").sort({
             date: 'desc'
         });
         return successResMsg(res, 200, Logs);
@@ -29,3 +32,31 @@ exports.getUserLog = async (req, res) => {
     }
 }
 
+exports.updateLog = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const id = decodedToken.id;
+
+        const logdetail = await Log.find({
+            _id: req.params.id
+        }).populate("user").populate("product");
+        const log = await Log.findByIdAndUpdate(req.params.id, {
+            status: "true",
+        }, {
+            new: true,
+        });
+
+
+        const logObject = {
+            user: id,
+            action: "Confirmed Product",
+            product: logdetail[0].product._id,
+            quantity: logdetail[0].product.quantity,
+        }
+        const log1 = await Log.create(logObject);
+        return successResMsg(res, 200, log);
+    } catch (err) {
+        return errorResMsg(res, 500, err);
+    }
+}
